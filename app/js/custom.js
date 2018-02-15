ecd2e764018fbc222275194eb1dc2287c34c05c1
Alexandr <alexandrfedorenkoooo@gmail.com>
2018-02-15 17:02:39 +0200

    add hrader top

--- app/js/custom.js
+++ app/js/custom.js
@@ -3,4 +3,13 @@ $(function() {
     $("img, a").on("dragstart", function(event) { event.preventDefault(); });
 });
 
+//header-top
+$("#open").click(function (e) {
+    e.preventDefault();
+    if (screen.width <= '768'){
+        $(".magaz-dropdown").slideToggle();
+    }
+});
+
+
 
