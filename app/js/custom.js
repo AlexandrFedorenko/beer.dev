09f237786b1d4ea4cb130fba485fbe4cdc1dbc60
Alexandr <alexandrfedorenkoooo@gmail.com>
2018-02-16 15:07:18 +0200

    add header

--- app/js/custom.js
+++ app/js/custom.js
@@ -4,12 +4,24 @@ $(function() {
 });
 
 //header-top
-$("#open").click(function (e) {
+$(".open").click(function (e) {
     e.preventDefault();
     if (screen.width <= '768'){
-        $(".magaz-dropdown").slideToggle();
+        $(this).find(".magaz-dropdown").slideToggle();
     }
 });
 
+//burger
+$(".burger-menu").click(function () {
+    $(this).toggleClass("menu-on");
+    $('.menu').slideToggle();
+    $('.header').toggleClass('bg');
+});
 
-
+//slider main
+$('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplaySpeed:300
+})
